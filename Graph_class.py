1141025ec9404bbac42a3703bdadedcf01bd4bde
Plumeline <151142975+Plumeline@users.noreply.github.com>
2026-03-14 13:05:53 +0100

    function display graph
    display of graph + minor bug correction

--- Graph_class.py
+++ Graph_class.py
@@ -1,8 +1,35 @@
-class DirectedWeightedGrapgh :
-    def __init__(self, nb_vertices, nb_edges, vertices, edges, weights):
-        self.nb_vertices = nb_vertices
-        self.nb_edges = nb_edges
+class DirectedWeightedGraph :
+    def __init__(self, vertices, edges, weights):
+
         self.vertices = vertices # Simple list
         self.edges = edges # Set of couple (vertex1, vertex2) representing a directed edge from vertex1 to vertex2
         self.weights = weights # Dictionnary associating each couple from the list of edges to a weight
+        self.nb_vertices = len(self.vertices)
+        self.nb_edges = len(self.edges)
+
+    def display_graph(self):
+        print("  ", end="")
+        for i in range (self.nb_vertices):
+            print(self.vertices[i], end=" ")
+        
+        print("")
+        
+        for j in range(self.nb_vertices):
+            print(self.vertices[j], end=" ")
+            
+            for x in range (self.nb_vertices):
+                if ((j, x) in self.edges):
+                    print(self.weights[(j,x)], end=" ")
+                else:
+                    print("0", end=" ")
+            
+            print("")
+
+
+vertices = [0, 1, 2]
+edges = [(0,1), (1,2)]
+weights = {(0,1) : 2, (1,2) : 4}
+graph1 = DirectedWeightedGraph(vertices, edges, weights)  
+graph1.display_graph()
+
 
