5362993ac272611344537d985340f6d0f962a596
Plumeline <151142975+Plumeline@users.noreply.github.com>
2026-03-21 18:29:17 +0100

    execution traces code
    created a portion of code to automatically create/update all execution traces

--- main.py
+++ main.py
@@ -102,4 +102,79 @@ def main():
 
 
 if __name__ == "__main__":
+
+    #if you want to update execution_traces, or re-create the files, just set this to True and run the main
+    execution_traces = False 
+
+
+
+    if execution_traces :
+
+        for graph_num in range (1, k+1):
+        
+            graph = DirectedWeightedGraph()
+            graph.readFile(f"./Graphs/graph"+ str(graph_num) + ".txt")
+            list_L, list_P = graph.floydWarshall()
+
+            with open("./Exec_traces/graph"+ str(graph_num) + ".txt", "w", encoding="utf-8") as g:
+                for i in range(graph.nb_vertices + 1): # the number of iteration
+                                g.write(f"Iteration {i} \n")
+
+                                g.write(f" "*10)
+                                w = 5
+                                for v in range(graph.nb_vertices):
+                                    g.write(f"{'':>{(w+1)//2 - len(str(v))//2 - 1}}")
+                                    g.write(str(v))
+                                    g.write(f"{'':>{w - (w+1)//2 -len(str(v))//2}}")
+
+                                g.write(" "*14)
+
+                                for v in range(graph.nb_vertices):
+                                    g.write(f"{'':>{(w+1)//2 - len(str(v))//2 - 1}}")
+                                    g.write(str(v))
+                                    g.write(f"{'':>{w - (w+1)//2 -len(str(v))//2}}")
+                                g.write("\n\n")
+
+                                g.write(" "*9 + "+" + "-"*(w*graph.nb_vertices) + "+" + " "*12 + "+" + "-"*(w*graph.nb_vertices) + "+" + "\n")
+
+                                for v in range(graph.nb_vertices):
+                                    if v+1 == (graph.nb_vertices + 1)//2:
+                                        g.write(" L :")
+                                    else:
+                                        g.write("    ")
+
+                                    g.write(" "*(4-len(str(v))) + str(v) + " |")
+                                    for elem in range(graph.nb_vertices):
+                                        weight = list_L[i][v][elem]
+                                        size = len(str(weight))
+                                        g.write(f"{'':>{(w-size)//2}}")
+                                        g.write(str(weight))
+                                        g.write(f"{'':>{(w-size)//2 + (size+1)%2}}")
+                                    
+                                    g.write("|   ")
+                                    if v+1 == (graph.nb_vertices + 1)//2:
+                                        g.write(" P :")
+                                    else:
+                                        g.write("    ")
+                                    
+                                    g.write(" "*(4-len(str(v))) + str(v) + " |")
+                                    for elem in range(graph.nb_vertices):
+                                        path = list_P[i][v][elem]
+                                        size = len(str(path))
+                                        g.write(f"{'':>{(w-size)//2}}")
+                                        g.write(str(path))
+                                        g.write(f"{'':>{(w-size)//2 + (size+1)%2}}")
+                                    g.write("|\n")
+                                g.write(" "*9 + "+" + "-"*(w*graph.nb_vertices) + "+" + " "*12 + "+" + "-"*(w*graph.nb_vertices) + "+\n")
+                                g.write("\n")
+
+                                if (graph.has_absorbant_cycle(list_L[len(list_L)-1])):
+                                    g.write(" This graph contains at least one absorbant cycle ! \n \n")
+                                else:
+                                    g.write (" This graph contains no absorbant cycle. \n \n")
+
+
+
     main()
+
+    
